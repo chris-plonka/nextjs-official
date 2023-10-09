2b490a504744cb52ae4798cc4b164f54181bedc3
Krzysztof <info@krzysztofplonka.pl>
2023-10-09 21:23:40 +0200

    post hero

--- components/post/post-hero.tsx
+++ components/post/post-hero.tsx
@@ -10,7 +10,7 @@ interface PostHeroProps {
 const PostHero = ({ post }: PostHeroProps) => {
     return (
         < div >
-        
+
             <PostContent isPostPage post={post} />
             <Image className="rounded-md object-cover object-center h-[300px] md:h-[500px] mt-6"
                 src={`${process.env.NEXT_PUBLIC_ASSETS_URL}${post.image}?key=optimised`}
